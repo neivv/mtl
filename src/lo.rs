e94abc79235c6dab286a95ba3518d5c1ecc9978b
Markus Heikkinen <ittevien@gmail.com>
2025-08-30 17:34:07 +0300

    Fix extended dats breaking bunker overlays, also fix old overlay reading code being incorrect past frame 0

--- src/lo.rs
+++ src/lo.rs
@@ -15,6 +15,41 @@ pub unsafe fn images_dat_attack_overlay(
     image: *mut bw::Image,
     direction: u8,
 ) -> Option<(i8, i8)> {
+    let image_id = (*image).image_id;
+    let frame = (*image).frame;
+    if let Some(ext_array) = bw_dat::extended_array(0x11) {
+        // Read from samase extdat overlays
+        let index = image_id as usize * 6 + 1;
+        let start = ext_array.pointer.cast::<usize>();
+        let len = ext_array.end.cast::<usize>().offset_from_unsigned(start);
+        if index >= len {
+            error!("No attack overlay of image {image_id:x}");
+            return None;
+        }
+        let pointer = *start.add(index) as *const u8;
+        if pointer.is_null() {
+            error!("No attack overlay of image {image_id:x}");
+            return None;
+        }
+
+        let frame_count = *pointer.cast::<u32>();
+        let variation_count = *pointer.add(4).cast::<u32>();
+        if frame as u32 >= frame_count || direction as u32 >= variation_count {
+            return None;
+        }
+        let offset = *pointer.add(8).cast::<u32>().add(frame as usize);
+        let offset = offset.checked_add(direction as u32 * 2)? as usize;
+
+        let mut x = *pointer.add(offset) as i8;
+        let y = *pointer.add(offset).add(1) as i8;
+        if (*image).flags & 0x2 != 0 {
+            x = 0i8.wrapping_sub(x);
+        }
+        return Some((x, y));
+    }
+
+    // No ext dat, open images.dat and parse from there
+
     let mut images_tbl = images_tbl().borrow_mut();
     let images_tbl = images_tbl.get_or_insert_with(|| {
         samase::read_file("arr\\images.tbl").expect("Couldn't read images.tbl")
@@ -39,7 +74,6 @@ pub unsafe fn images_dat_attack_overlay(
     if overlays.is_empty() {
         *overlays = (0..999).map(|_| None).collect();
     }
-    let image_id = (*image).image_id;
     let tbl_index = ids[image_id as usize];
     if tbl_index == 0 {
         error!("No attack overlay for image {:x}", image_id);
@@ -59,15 +93,14 @@ pub unsafe fn images_dat_attack_overlay(
         overlays[image_id as usize] = Some(data);
     }
     let lo_data = overlays[image_id as usize].as_ref().unwrap();
-    let offset = lo_data.get(8 + (*image).frame as usize..)?.read_u32::<LE>().ok()? as usize;
+    let offset = lo_data.get(8 + 4 * frame as usize..)?.read_u32::<LE>().ok()? as usize;
     let data = lo_data.get(offset.checked_add(direction as usize * 2)?..)?.get(..2)?;
-    let x = data[0] as i8;
+    let mut x = data[0] as i8;
     let y = data[1] as i8;
     if (*image).flags & 0x2 != 0 {
-        Some((0i8.saturating_sub(x), y))
-    } else {
-        Some((x, y))
+        x = 0i8.wrapping_sub(x);
     }
+    Some((x, y))
 }
 
 fn tbl_string(data: &[u8], index: u16) -> Option<&str> {
